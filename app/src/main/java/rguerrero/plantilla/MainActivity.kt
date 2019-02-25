d5e5e2dc0338c307bdc0c5c87a38d7befb229ba2
Roberto Guerrero <rguerreroayon@gmail.com>
2019-02-24 17:01:32 -0700

    First Commit

---
+++ app/src/main/java/rguerrero/plantilla/MainActivity.kt
@@ -0,0 +1,12 @@
+package rguerrero.plantilla
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
